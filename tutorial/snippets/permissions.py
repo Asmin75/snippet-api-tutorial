5b8a42f8ee281b84baa950deced733806911e739
Asmin75 <asminrai7@gmail.com>
2019-08-26 11:33:59 +0545

    permission given with respected users

--- tutorial/snippets/permissions.py
+++ tutorial/snippets/permissions.py
@@ -8,3 +8,7 @@ class IsOwnerOrReadonly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
         # so always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.owner == request.user
